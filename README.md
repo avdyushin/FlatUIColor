0c2e157bdbe1bca21f8832bc5ec40962aaed4827
Grigory Avdyushin <avdyushin.g@gmail.com>
2019-02-13 19:11:06 +0100

    Refactoring

--- README.md
+++ README.md
@@ -1,35 +1,58 @@
-# Flat UI Colors swift extension
+# UIColor extension
+## Includes some Flat UI color schemes
 
-Project has been moved to [SwiftExtensions](https://github.com/avdyushin/SwiftExtensions)
+This extension brings some convenience initializers into UIColor object and useful methods and calculated variables.
 
-###### Usage
+### Create color with CSS hex string
+
+```swift
+let red = UIColor(string: "#FF0000")
+```
+
+### Create color with hex integer representation
+
+```swift
+let green = UIColor(hex: 0x00FF00)
+```
+
+### Convert color to hex integer representation
+
+```swift
+let hex = UIColor.blue.asInt
+// hex = 255
+```
+
+### Convert color to CSS hex string representation
+
+```swift
+let string = UIColor.yellow.asHexString
+// string = "#ffff00"
+```
+
+### Make color lighter
+
+```swift
+let lightOrange = UIColor.orange.lighterColor
+// "#ff9f3f"
+```
+
+### Make color darker
+
+```swift
+let darkOrange = UIColor.orange.darkerColor
+// "#bf5f00"
+```
+
+### Flat UI colors
+
+```swift
+let greenSea = UIColor.flatGreenSea
+let veryBerry = UIColor.flatVeryBerry
+```
+
+#### Usage
 
 Add `UIColor.swift` into your project
 
-Code sample: `UIColor.flatWisteriaColor`
-
-###### Full color list
-
-* flatTurquoiseColor 
-* flatGreenSeaColor 
-* flatEmeraldColor 
-* flatNephritisColor 
-* flatPeterRiverColor 
-* flatBelizeHoleColor 
-* flatAmethystColor 
-* flatWisteriaColor 
-* flatWetAsphaltColor 
-* flatMidnightBlueColor 
-* flatSunFlowerColor 
-* flatOrangeColor 
-* flatCarrotColor 
-* flatPumkinColor 
-* flatAlizarinColor 
-* flatPomegranateColor 
-* flatCloudsColor 
-* flatSilverColor 
-* flatAsbestosColor 
-* flatConcerteColor 
-
-###### Flat UI Colors
+#### Links
 [http://flatuicolors.com/](http://flatuicolors.com/)
--- UIColor.swift
+++ UIColor.swift
@@ -3,7 +3,7 @@
 //  Flat UI Colors
 //
 //  Created by Grigory Avdyushin on 22.01.15.
-//  Copyright (c) 2015-2017 Grigory Avdyushin. All rights reserved.
+//  Copyright (c) 2015-2019 Grigory Avdyushin. All rights reserved.
 //
 
 import UIKit
@@ -24,13 +24,14 @@ extension UIColor {
     }
 
     /// Returns color with given hex string
-    convenience init?(string: String) {
+    convenience init(string: String) {
         let string = string.replacingOccurrences(of: "#", with: "")
 
         guard
             let hex = Int(string, radix: 16),
             let format = ColorFormat(bitsCount: string.count * 4) else {
-                return nil
+                self.init(white: 0, alpha: 0)
+                return
         }
 
         self.init(hex: hex, format: format)
@@ -63,10 +64,10 @@ extension UIColor {
         }
 
         self.init(
-            red: CGFloat(red)/255.0,
-            green: CGFloat(green)/255.0,
-            blue: CGFloat(blue)/255.0,
-            alpha: CGFloat(alpha)/255.0
+            red: CGFloat(red) / 255.0,
+            green: CGFloat(green) / 255.0,
+            blue: CGFloat(blue) / 255.0,
+            alpha: CGFloat(alpha) / 255.0
         )
     }
 
@@ -84,74 +85,96 @@ extension UIColor {
         return String(format:"#%06x", asInt)
     }
 
+    /// Returns color with adjusted saturation or/and brightness
+    func withAdjusted(saturation: CGFloat = 0.0, brightness: CGFloat = 0.0) -> UIColor {
+        var h: CGFloat = 0, s: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
+
+        guard getHue(&h, saturation: &s, brightness: &b, alpha: &a) else {
+            return self
+        }
+
+        return UIColor(
+            hue: h,
+            saturation: min(max(s + saturation, 0.0), 1.0),
+            brightness: min(max(b + brightness, 0.0), 1.0),
+            alpha: a
+        )
+    }
+
+    /// Returns lighter color by 25%
+    var lighterColor: UIColor {
+        return self.withAdjusted(saturation: -0.25)
+    }
+
+    /// Return darker color by 25%
+    var darkerColor: UIColor {
+        return self.withAdjusted(brightness: -0.25)
+    }
 }
 
-/// Colors v1.0
+/// Flat UI Palette v1
 /// https://flatuicolors.com/palette/defo
 extension UIColor {
-    // green sea
-    static let flatTurquoiseColor = UIColor(hex: 0x1ABC9C)
-    static let flatGreenSeaColor = UIColor(hex: 0x16A085)
-    // green
-    static let flatEmeraldColor = UIColor(hex: 0x2ECC71)
-    static let flatNephritisColor = UIColor(hex: 0x27AE60)
-    // blue
-    static let flatPeterRiverColor = UIColor(hex: 0x3498DB)
-    static let flatBelizeHoleColor = UIColor(hex: 0x2980B9)
-    // purple
-    static let flatAmethystColor = UIColor(hex: 0x9B59B6)
-    static let flatWisteriaColor = UIColor(hex: 0x8E44AD)
-    // dark blue
-    static let flatWetAsphaltColor = UIColor(hex: 0x34495E)
-    static let flatMidnightBlueColor = UIColor(hex: 0x2C3E50)
-    // yellow
-    static let flatSunFlowerColor = UIColor(hex: 0xF1C40F)
-    static let flatOrangeColor = UIColor(hex: 0xF39C12)
-    // orange
-    static let flatCarrotColor = UIColor(hex: 0xE67E22)
-    static let flatPumkinColor = UIColor(hex: 0xD35400)
-    // red
-    static let flatAlizarinColor = UIColor(hex: 0xE74C3C)
-    static let flatPomegranateColor = UIColor(hex: 0xC0392B)
-    // white
-    static let flatCloudsColor = UIColor(hex: 0xECF0F1)
-    static let flatSilverColor = UIColor(hex: 0xBDC3C7)
-    // gray
-    static let flatAsbestosColor = UIColor(hex: 0x7F8C8D)
-    static let flatConcerteColor = UIColor(hex: 0x95A5A6)
+
+    // Green / Sea
+    static let flatTurquoise = UIColor(hex: 0x1ABC9C)
+    static let flatGreenSea = UIColor(hex: 0x16A085)
+    // Green
+    static let flatEmerald = UIColor(hex: 0x2ECC71)
+    static let flatNephritis = UIColor(hex: 0x27AE60)
+    // Blue
+    static let flatPeterRiver = UIColor(hex: 0x3498DB)
+    static let flatBelizeHole = UIColor(hex: 0x2980B9)
+    // Purple
+    static let flatAmethyst = UIColor(hex: 0x9B59B6)
+    static let flatWisteria = UIColor(hex: 0x8E44AD)
+    // Dark blue
+    static let flatWetAsphalt = UIColor(hex: 0x34495E)
+    static let flatMidnightBlue = UIColor(hex: 0x2C3E50)
+    // Yellow
+    static let flatSunFlower = UIColor(hex: 0xF1C40F)
+    static let flatOrange = UIColor(hex: 0xF39C12)
+    // Orange
+    static let flatCarrot = UIColor(hex: 0xE67E22)
+    static let flatPumkin = UIColor(hex: 0xD35400)
+    // Red
+    static let flatAlizarin = UIColor(hex: 0xE74C3C)
+    static let flatPomegranate = UIColor(hex: 0xC0392B)
+    // White
+    static let flatClouds = UIColor(hex: 0xECF0F1)
+    static let flatSilver = UIColor(hex: 0xBDC3C7)
+    // Gray
+    static let flatAsbestos = UIColor(hex: 0x7F8C8D)
+    static let flatConcerte = UIColor(hex: 0x95A5A6)
 }
 
-/// Dutch Pallete
+/// Dutch Palette
 /// https://flatuicolors.com/palette/nl
 extension UIColor {
 
     // Yellow / Red
-    static let fd_Sunflower = UIColor(hex: 0xFFC312)
-    static let fd_RadianYellow = UIColor(hex: 0xF79F1F)
-    static let fd_PuffinsBull = UIColor(hex: 0xEE5A24)
-    static let fd_RedPigment = UIColor(hex: 0xEA2027)
-
+    static let flatSunflower = UIColor(hex: 0xFFC312)
+    static let flatRadianYellow = UIColor(hex: 0xF79F1F)
+    static let flatPuffinsBull = UIColor(hex: 0xEE5A24)
+    static let flatRedPigment = UIColor(hex: 0xEA2027)
     // Green
-    static let fd_Energos = UIColor(hex: 0xC4E538)
-    static let fd_AndroidGreen = UIColor(hex: 0xA3CB38)
-    static let fd_PixelatedGrass = UIColor(hex: 0x009432)
-    static let fd_TurkishAqua = UIColor(hex: 0x006266)
-
+    static let flatEnergos = UIColor(hex: 0xC4E538)
+    static let flatAndroidGreen = UIColor(hex: 0xA3CB38)
+    static let flatPixelatedGrass = UIColor(hex: 0x009432)
+    static let flatTurkishAqua = UIColor(hex: 0x006266)
     // Blue
-    static let fd_BlueMartina = UIColor(hex: 0x12CBC4)
-    static let fd_MediterraneanSea = UIColor(hex: 0x1289A7)
-    static let fd_MerchantMarineBlue = UIColor(hex: 0x0652DD)
-    static let fd_20000LeaguasUnderTheSea = UIColor(hex: 0x1B1464)
-
+    static let flatBlueMartina = UIColor(hex: 0x12CBC4)
+    static let flatMediterraneanSea = UIColor(hex: 0x1289A7)
+    static let flatMerchantMarineBlue = UIColor(hex: 0x0652DD)
+    static let flat20000LeaguesUnderTheSea = UIColor(hex: 0x1B1464)
     // Rose / Purpule
-    static let fd_LavenderRose = UIColor(hex: 0xFDA7DF)
-    static let fd_LavenderTea = UIColor(hex: 0xD980FA)
-    static let fd_ForgottenPurple = UIColor(hex: 0x9980FA)
-    static let fd_CircumorbitalRing = UIColor(hex: 0x5758BB)
-
+    static let flatLavenderRose = UIColor(hex: 0xFDA7DF)
+    static let flatLavenderTea = UIColor(hex: 0xD980FA)
+    static let flatForgottenPurple = UIColor(hex: 0x9980FA)
+    static let flatCircumorbitalRing = UIColor(hex: 0x5758BB)
     // Rose / Red
-    static let fd_BaraRed = UIColor(hex: 0xED4C67)
-    static let fd_VeryBerry = UIColor(hex: 0xB53471)
-    static let fd_Hollyhock = UIColor(hex: 0x833471)
-    static let fd_MargentaPurple = UIColor(hex: 0x6F1E51)
+    static let flatBaraRed = UIColor(hex: 0xED4C67)
+    static let flatVeryBerry = UIColor(hex: 0xB53471)
+    static let flatHollyhock = UIColor(hex: 0x833471)
+    static let flatMargentaPurple = UIColor(hex: 0x6F1E51)
 }
